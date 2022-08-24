4d5b3ad98026d6a5e1e3bd9754233749bc8a04d9
Borisivc <101225512+Borisivc@users.noreply.github.com>
2022-08-24 18:13:24 -0400

    nuevo proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AgendaMovil',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
