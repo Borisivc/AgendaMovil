2eb36ad1889dd7d441639d826603366d76e7e37a
Borisivc <101225512+Borisivc@users.noreply.github.com>
2022-08-26 18:11:10 -0400

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,10 +19,7 @@ const routes: Routes = [
     path: 'menu',
     loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
   },
-  {
-    path: 'sub-menu',
-    loadChildren: () => import('./sub-menu/sub-menu.module').then( m => m.SubMenuPageModule)
-  },
+  
 ];
 
 @NgModule({
